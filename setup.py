47048faf3705501a3ca61d3c67405f0976e1257b
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2021-09-10 09:42:04 -0400

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/nasifimtiazohi/version_differ",
-    version="0.0.3",
+    version="version='0.0.4'",
     zip_safe=False,
 )
