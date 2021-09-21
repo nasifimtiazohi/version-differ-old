cdd65e42d1e2c15061d4fb53240d9908a5d93b90
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2021-09-21 18:57:30 -0400

    Bump version: 0.4.3 → 0.5.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/nasifimtiazohi/version_differ",
-    version="version='0.4.3'",
+    version="version='0.5.0'",
     zip_safe=False,
 )
