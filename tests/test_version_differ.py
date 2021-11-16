7625cfb549a77c229fca421da3dd456e79a58261
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2021-11-16 17:22:33 -0500

    remove boilerplate fixture in test code

--- tests/test_version_differ.py
+++ tests/test_version_differ.py
@@ -13,22 +13,6 @@ from pygit2 import clone_repository
 from version_differ.version_differ import *
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 def test_src_download_url():
     assert (
         get_package_version_source_url(CARGO, "depdive", "0.1.0")
